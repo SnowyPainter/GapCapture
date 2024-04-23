b26a534901aa89a0dc7ac20a27a7ce8270b94d7c
Minwoo Yu <snowypainter@gmail.com>
2024-04-23 13:18:32 +0900

    Find gap

--- market_gap.py
+++ market_gap.py
@@ -1,7 +1,31 @@
 import stockdata
 
-df = stockdata.create_dataset(["nvda", "msft"], stockdata.today_before(100), stockdata.today(),"1d")
-df = stockdata.normalize(df)
+class GapFinder:
+    def _normalize(self):
+        mean, std = self.raw.mean(), self.raw.std()
+        self.normalized_data = (self.raw - mean) / std
+    
+    def __init__(self, symbol1, symbol2, start, end, interval):
+        self.symbols = [symbol1, symbol2]
+        self.raw = stockdata.create_dataset(self.symbols, start, end, interval)
+        self._normalize()
+    
+    def append_raw(self, df):
+        self.raw.append(df)
+        self._normalize()
+        
+    def get_buy_list(self, threshold):
+        symbol1_price = self.normalized_data[self.symbols[0]+"_Price"]
+        symbol2_price = self.normalized_data[self.symbols[1]+"_Price"]
+        diff = symbol1_price - symbol2_price
+        diff = diff.loc[abs(diff) >= threshold]
+        return diff.map(lambda x: 1 if x < 0 else 1)
+        
+    def corr(self):
+        return self.normalized_data.corr()
 
-corr = df.corr()
-print(corr)
+tickers = ["nvda", "000660.KS"]
+stream = GapFinder("nvda", "000660.KS", stockdata.today_before(100), stockdata.today(),"1d")
+stream.append_raw(stockdata.create_realtime_dataset(tickers))
+
+print(stream.get_buy_list(0.3))
--- stockdata.py
+++ stockdata.py
@@ -14,6 +14,10 @@ def merge_dfs(dfs, on):
         merged = merged.merge(dfs[i], on=on)
     return merged
 
+def get_realtime_price(ticker):
+    t = yf.Ticker(ticker)
+    return t.info.get('currentPrice')
+
 def get_symbol_historical(symbol, start, end, period,interval):
     d = yf.download(symbol, start=start, end=end, period=period, interval=interval)
     d.rename(columns={'Open': symbol+'_Price'}, inplace=True)
@@ -26,6 +30,14 @@ def create_dataset(symbols, start, end, interval):
     for symbol in symbols:
         dfs.append(get_symbol_historical(symbol, start=start, end=end, period="max", interval=interval))
     return merge_dfs(dfs, on=dfs[0].index.name)
+
+def create_realtime_dataset(tickers, tz='America/New_York'):
+    df = pd.DataFrame()
+    for ticker in tickers:
+        df[ticker+'_Price'] = [get_realtime_price(ticker)]
+    df['Datetime'] = [pd.to_datetime(today(tz=tz), format="%Y-%m-%d %H:%M:%S%z")]
+    df.set_index('Datetime', inplace=True)
+    return df
 
 def normalize(df):
     m, std = df.mean(), df.std()
