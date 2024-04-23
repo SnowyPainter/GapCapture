fa39eb9fec01f115380e167b3bab812694e41118
Minwoo Yu <snowypainter@gmail.com>
2024-04-23 21:03:59 +0900

    Bt

---
+++ backtest.py
@@ -0,0 +1,71 @@
+import stockdata
+import tensorflow as tf
+import numpy as np 
+
+def reshape(state):
+    return np.reshape(state, [1, 1, 2])
+
+class Strategy1:
+    def __init__(self, env, amount, fee):
+        self.agent = tf.keras.models.load_model("nvdask.keras")
+        self.env = env
+        self.amount = amount
+        self.fee = fee
+    
+    def get_price(self, bar):
+        prices = self.env.raw.iloc[bar]
+        prices = np.array(prices)
+        prices[1] /= 1250 # currency ratio
+        return prices
+    
+    def test(self):
+        self.units = 0
+        self.position = 0
+        self.trades = 0
+
+        self.entry_price = 0
+        
+        self.net_wealths = list()
+        bar = 1
+        
+        self.symbol1_balance = self.amount / 2
+        self.symbol2_balance = self.amount / 2
+        self.symbol1_units = 0
+        self.symbol2_units = 0
+        
+        while bar < len(self.env.normalized_data) - 1:
+            prices = self.get_price(bar)
+            state = reshape(np.array([self.env.get_state(bar)]))
+            action = np.argmax(self.agent.predict(state, verbose=0)[0, 0])
+            # if 1
+            # symbol 2 sell, symbol 1 buy
+            # if 2
+            # symbol 2 buy, symbol 1 sell
+            # if 0 -> hold
+            
+            if action != 0:
+                if action == 1:
+                    if self.symbol2_units > 0:
+                        self.symbol2_balance += self.symbol2_units * prices[1]
+                        self.symbol2_units -= self.symbol2_units
+                        self.trades += 1
+                    symbol1_amount = int(self.symbol1_balance / prices[0])
+                    if symbol1_amount > 0:
+                        self.symbol1_balance -= symbol1_amount * prices[0] + symbol1_amount * self.fee
+                        self.symbol1_units += symbol1_amount
+                        self.trades += 1
+                elif action == 2:
+                    if self.symbol1_units > 0:
+                        self.symbol1_balance += self.symbol1_units * prices[0]
+                        self.symbol1_units -= self.symbol1_units
+                        self.trades += 1
+                    symbol2_amount = int(self.symbol2_balance / prices[1])
+                    if symbol2_amount > 0:
+                        self.symbol2_balance -= symbol2_amount * prices[1] + symbol2_amount * self.fee
+                        self.symbol2_units += symbol2_amount
+                        self.trades += 1
+            
+            self.net_wealths.append(self.symbol1_units * prices[0] + self.symbol2_units * prices[1] + self.symbol1_balance + self.symbol2_balance)
+            self.units = self.symbol1_units + self.symbol2_units
+    
+            bar += 1
--- market_gap.py
+++ market_gap.py
@@ -1,31 +1,21 @@
-import stockdata
-
-class GapFinder:
-    def _normalize(self):
-        mean, std = self.raw.mean(), self.raw.std()
-        self.normalized_data = (self.raw - mean) / std
-    
-    def __init__(self, symbol1, symbol2, start, end, interval):
-        self.symbols = [symbol1, symbol2]
-        self.raw = stockdata.create_dataset(self.symbols, start, end, interval)
-        self._normalize()
-    
-    def append_raw(self, df):
-        self.raw.append(df)
-        self._normalize()
-        
-    def get_buy_list(self, threshold):
-        symbol1_price = self.normalized_data[self.symbols[0]+"_Price"]
-        symbol2_price = self.normalized_data[self.symbols[1]+"_Price"]
-        diff = symbol1_price - symbol2_price
-        diff = diff.loc[abs(diff) >= threshold]
-        return diff.map(lambda x: 1 if x < 0 else 1)
-        
-    def corr(self):
-        return self.normalized_data.corr()
+import stockdata, learn
+import backtest
+import matplotlib.pyplot as plt
 
 tickers = ["nvda", "000660.KS"]
-stream = GapFinder("nvda", "000660.KS", stockdata.today_before(100), stockdata.today(),"1d")
-stream.append_raw(stockdata.create_realtime_dataset(tickers))
+env = learn.MarketEnvironment("nvda", "000660.KS", stockdata.today_before(180), stockdata.today(),"1d")
 
-print(stream.get_buy_list(0.3))
+def learn():
+    agent = learn.DQNAgent(env, 10000, 64)
+    agent.learn(30)
+    agent.save("./nvdask.keras")
+
+def bt():
+    amount = 10000
+    stgy = backtest.Strategy1(env, amount, 0.005)
+    stgy.test()
+    print(stgy.net_wealths[-1] / amount)
+    plt.plot(stgy.net_wealths)
+    plt.show()
+
+bt()
