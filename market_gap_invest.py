a7fdde5a69937d46da0cc7d10ca7e9ba93183c86
Minwoo Yu <snowypainter@gmail.com>
2024-05-23 21:29:37 +0900

    Fix bug

--- market_gap_invest.py
+++ market_gap_invest.py
@@ -19,7 +19,7 @@ def get_amount_of_sell(curr_units):
         n = SELL_AMOUNT
     return n
 def get_amount_of_buy(curr_amount, stock_price):
-    n = math.floor((curr_amount) / stock_price)
+    n = math.floor((curr_amount * (1-FEE)) / stock_price)
     if n > BUY_AMOUNT:
         n = BUY_AMOUNT
     return n
@@ -57,26 +57,6 @@ def create_prices(symbol1, symbol2):
     return df
 def reshape(state):
     return np.reshape(state, [1, 1, 2])
-def buy(logger, units, symbol, price, fee):
-    buy_order(symbol, units)
-    if symbol == symbols[0]:
-        symbol1_entry_price = set_entry_price(symbol1_entry_price, units, symbol1_price)
-    elif symbol == symbols[1]:
-        symbol2_entry_price = set_entry_price(symbol2_entry_price, units, symbol2_price)
-    current_amount -= units * price * (1+fee)
-    logger.log(f"BUY {symbol} - {units} / {price}")
-    trades += 1
-    return units
-def sell(logger, symbol, price):
-    if symbol == symbols[0]:
-        units = get_amount_of_sell(symbol1_units)
-    elif symbol == symbols[1]:
-        units = get_amount_of_sell(symbol2_units)
-    sell_order(symbol, units)
-    current_amount += units * price
-    logger.log(f"SOLD {symbol} - {units} / {price}")
-    trades += 1
-    return units
 
 t = input("실전투자(y), 모의투자(n) : ")
 k = REAL_KEY
@@ -126,22 +106,49 @@ logger.log(f"보유 종목 : {stocks_qty}")
 symbol1_entry_price = 0
 symbol2_entry_price = 0
 
+def buy(logger, units, symbol, price, fee):
+    global symbol1_entry_price, symbol2_entry_price, current_amount, trades
+    buy_order(symbol, units)
+    if symbol == symbols[0]:
+        symbol1_entry_price = set_entry_price(symbol1_entry_price, units, price)
+    elif symbol == symbols[1]:
+        symbol2_entry_price = set_entry_price(symbol2_entry_price, units, price)
+    current_amount -= units * price * (1+fee)
+    logger.log(f"BUY {symbol} - {units} / {price}")
+    trades += 1
+    return units
+def sell(logger, symbol, price, loss):
+    global current_amount, trades
+    if symbol == symbols[0]:
+        units = get_amount_of_sell(symbol1_units)
+    elif symbol == symbols[1]:
+        units = get_amount_of_sell(symbol2_units)
+    sell_order(symbol, units)
+    current_amount += units * price
+    logger.log(f"SOLD {symbol} - {units} / {price}, Profit : {loss}")
+    trades += 1
+    return units
+
 while True:
     current_time = datetime.datetime.now().time()
-    if start_time <= current_time <= end_time:
+    if True:#start_time <= current_time <= end_time:
         prices = create_prices(symbols[0], symbols[1])
         env.append_raw(prices)
         state = reshape(np.array([env.get_last()]))
         symbol1_price = prices.iloc[0][0]
         symbol2_price = prices.iloc[0][1]
         
-        symbol1_loss = (symbol1_price - symbol1_entry_price) / symbol1_entry_price
-        symbol2_loss = (symbol2_price - symbol2_entry_price) / symbol2_entry_price
         
-        if symbol1_units > 0 and symbol1_loss >= TAKE_PROFIT:
-            symbol1_units -= sell(logger, symbols[0], symbol1_price)
-        if symbol2_units > 0 and symbol2_loss >= TAKE_PROFIT:
-            symbol2_units -= sell(logger, symbols[1], symbol2_price)
+        symbol1_loss = ((symbol1_price - symbol1_entry_price) / symbol1_entry_price) if symbol1_entry_price != 0 else 0
+        symbol2_loss = ((symbol2_price - symbol2_entry_price) / symbol2_entry_price) if symbol2_entry_price != 0 else 0
+        
+        if symbol1_units > 0:
+            if symbol1_loss >= TAKE_PROFIT or symbol1_loss <= STOP_LOSS:
+                symbol1_units -= sell(logger, symbols[0], symbol1_price, symbol1_loss)
+                
+        if symbol2_units > 0:
+            if symbol2_loss >= TAKE_PROFIT or symbol2_loss <= STOP_LOSS:
+                symbol2_units -= sell(logger, symbols[1], symbol2_price, symbol2_loss)
             
         if symbol1_units <= 0:
             symbol1_entry_price = 0
@@ -154,20 +161,20 @@ while True:
         else:
             if action == 1:
                 if symbol2_units > 0:
-                    symbol2_units -= sell(logger, symbols[1], symbol2_price)
+                    symbol2_units -= sell(logger, symbols[1], symbol2_price, symbol2_loss)
                 units = get_amount_of_buy(current_amount, symbol1_price)
                 if units > 0:
                     symbol1_units += buy(logger, units, symbols[0], symbol1_price, fee)
                 else:
-                    logger.log(f"No Money to buy {symbols[0]} - {current_amount}")
+                    logger.log(f"No Money to buy {symbols[0]} - {current_amount} / {symbol1_price}")
             elif action == 2:
                 if symbol1_units > 0:
-                    symbol1_units -= sell(logger, symbols[0], symbol1_price)
+                    symbol1_units -= sell(logger, symbols[0], symbol1_price, symbol1_loss)
                 units = get_amount_of_buy(init_amount, symbol2_price)
                 if units > 0:
                     symbol2_units += buy(logger, units, symbols[1], symbol2_price, fee)
                 else:
-                    logger.log(f"No Money to buy {symbols[1]} - {current_amount}")
+                    logger.log(f"No Money to buy {symbols[1]} - {current_amount} / {symbol2_price}")
         net_wealths.append(symbol1_units * symbol1_price + symbol2_units * symbol2_price + current_amount)
         time.sleep(60*5)
     else:
