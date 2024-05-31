cbc09280d2702fdffa6cc1655bf12d346b0e357b
Minwoo Yu <snowypainter@gmail.com>
2024-05-31 09:50:52 +0900

    Rename & readini.py

---
+++ readini.py
@@ -0,0 +1,18 @@
+import configparser
+
+def read(fname):
+    config = configparser.ConfigParser()
+    config.read(fname)
+    return {
+        "TAG1" : config['SETTINGS']['SYMBOL1_TAG'],
+        "TAG2" : config['SETTINGS']['SYMBOL2_TAG'],
+        "SELL_AMOUNT" : config['SETTINGS'].getint('SELL_AMOUNT'),
+        "BUY_AMOUNT" : config['SETTINGS'].getint('BUY_AMOUNT'),
+        "CODE1" : config['SETTINGS']['SYMBOL1'],
+        "CODE2" : config['SETTINGS']['SYMBOL2'],
+        "NAME1" : config['SETTINGS']['SYMBOL1_NAME'],
+        "NAME2" : config['SETTINGS']['SYMBOL2_NAME'],
+        "FEE" : config['SETTINGS'].getfloat('FEE'),
+        "TAKE_PROFIT" : config['SETTINGS'].getfloat('TAKE_PROFIT'),
+        "STOP_LOSS" : config['SETTINGS'].getfloat('STOP_LOSS')
+    }
